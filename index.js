4bd1de1d2fd3c5e3ad3eef9cb59bce18634d1917
Sean Owens <seancowens13@gmail.com>
2018-10-07 22:19:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine, newPerson) {
   katzDeliLine.push(newPerson);
   for (var i = 0; i < katzDeliLine.length; i++) {
-    return `Welcome, ${katzDeliLine[i]}. You are number ${i + 1} in line.`, i++;
+    return `Welcome, ${katzDeliLine[i]}. You are number ${i + 1} in line.`;
   }
 }
 
